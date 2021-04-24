335ff280c0a092a6490f446f0db3fb6a6724e83d
Nghiattk27 <79556094+Nghiattk27@users.noreply.github.com>
2021-04-24 12:45:54 +0700

    product

---
+++ temp.php
@@ -0,0 +1,26 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "cookstar";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT productName,buyPrice FROM products";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "<br> productname: ". $row["productName"]. " - price: ". $row["buyPrice"].  "<br>";
+}
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
